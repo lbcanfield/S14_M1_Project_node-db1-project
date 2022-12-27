111211f5739d60223d82e56f5bdff06e39a2f7ed
lbcanfield <lewie.b.canfield@gmail.com>
2022-12-27 17:54:14 -0600

    server.js - catch-all endpoint created.

--- api/server.js
+++ api/server.js
@@ -4,4 +4,10 @@ const server = express();
 
 server.use(express.json());
 
+server.use('*', (request, response) => {
+     response.status(404).json({
+          message: 'not found',
+     })
+})
+
 module.exports = server;
